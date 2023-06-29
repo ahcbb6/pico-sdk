2ce24b86274e37bc7e25c901a07be95724e9676f
Alejandro Enedino Hernandez Samaniego <alhe@linux.microsoft.com>
2023-06-28 18:56:11 -0600

    unset toolchain file
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <alhe@linux.microsoft.com>

--- cmake/preload/platforms/pico/pico.cmake
+++ cmake/preload/platforms/pico/pico.cmake
@@ -1,6 +1,7 @@
 pico_message("Loading pico.cmake.")
 message("TCFILE: ${CMAKE_TOOLCHAIN_FILE}")
 unset(CMAKE_TOOLCHAIN_FILE)
+message("TCFILE2: ${CMAKE_TOOLCHAIN_FILE}")
 if (NOT (DEFINED PICO_COMPILER OR DEFINED CMAKE_TOOLCHAIN_FILE))
   message("GPIB:Defaulting compiler")
     pico_message("Defaulting PICO platform compiler to pico_arm_gcc since not specified.")
